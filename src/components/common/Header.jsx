f212e8eb6358807579718dfda27e2d919d587697
unknown <zaidj777@gmail.com>
2023-06-07 00:35:55 +0530

    Completed the NavBar features

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,41 @@
 
 
-import {AppBar, Toolbar, styled } from '@mui/material';
+import {AppBar, Toolbar, styled, Box, Typography, InputBase} from '@mui/material';
+
+import { Menu, BookmarkAdd, ExpandMore } from '@mui/icons-material';
+
+
 import { logoURL } from '../../constants/constant';
 
 const StyledToolbar = styled(Toolbar)`
 background: #121212;
 min-height:56px !important;
 padding: 0 115px !important;
+justify-content: space-between;
+& > * {
+    padding: 0 16px;
+}
+& > div {
+    display: flex;
+    align-item: center;
+    cursor: pointer;
+    & > p {
+        font-size: 14px;
+        fontt-width: 600;
+    }
+}
+& > p {
+    font-size: 14px;
+    fontt-width: 600;
+
+}
+`;
+
+const InputSearchField = styled(InputBase)`
+ background: #FFFFFF;
+ height: 30px;
+ width: 53%;
+ border-radius: 5px;
 
 `
 
@@ -22,6 +51,22 @@ const Header = () => {
         <AppBar>
             <StyledToolbar>
                 <Logo src={logoURL} alt='logo' />
+                <Box>
+                    <Menu />
+                    <Typography>Menu</Typography>
+                </Box>
+                <InputSearchField />
+                <Typography>IMDB<Box component="span" >Pro</Box></Typography>
+                <Box>
+                    <BookmarkAdd />
+                    <Typography>watchlist</Typography>
+                </Box>
+
+                <Typography>Sign In</Typography>
+                <Box>
+                <Typography>En</Typography>
+                <ExpandMore />
+                </Box>
             </StyledToolbar>
         </AppBar>
     )
