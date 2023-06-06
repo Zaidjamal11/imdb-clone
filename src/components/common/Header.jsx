5d0ad35c1a9de4b3012735b58f52038ddabf354e
unknown <zaidj777@gmail.com>
2023-06-07 00:11:58 +0530

    NavBar feature Added

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,30 @@
+
+
+import {AppBar, Toolbar, styled } from '@mui/material';
+import { logoURL } from '../../constants/constant';
+
+const StyledToolbar = styled(Toolbar)`
+background: #121212;
+min-height:56px !important;
+padding: 0 115px !important;
+
+`
+
+
+const Logo = styled('img')({
+    width: 64
+})
+
+
+
+const Header = () => {
+    return (
+        <AppBar>
+            <StyledToolbar>
+                <Logo src={logoURL} alt='logo' />
+            </StyledToolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+//components
+import Header from "../components/common/Header";
+
+const Home = () => {
+    return (
+        <div>
+
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
